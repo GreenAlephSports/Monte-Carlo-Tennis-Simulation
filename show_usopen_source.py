25bccee40afbaf64a086e07a9bf63a393c4c9c8b
idan.levy-greenaleph <idan.levy@greenaleph.com>
2026-09-02 13:20:29 -0400

    Updated, curr vs pre odds

---
+++ show_usopen_source.py
@@ -0,0 +1,5 @@
+﻿import urllib.request, json
+url = "https://www.usopen.org/en_US/scores/feeds/2026/draws/MS.json"
+data = json.loads(urllib.request.urlopen(url).read())
+print(f"Loaded {len(data.get('matches', data))} entries from official usopen.org feed")
+print(json.dumps(data, indent=2)[:500])
